32f96b17b1ed18ade6ecf7f9d8aa3d5bf10b81db
twhittaker06 <t.whittaker@mmu.ac.uk>
2026-03-17 11:06:48 +0000

    Delete story_content directory

--- story_content/user.js
+++
@@ -1,9 +0,0 @@
-window.InitUserScripts = function()
-{
-var player = GetPlayer();
-var object = player.object;
-var once = player.once;
-var addToTimeline = player.addToTimeline;
-var setVar = player.SetVar;
-var getVar = player.GetVar;
-};
